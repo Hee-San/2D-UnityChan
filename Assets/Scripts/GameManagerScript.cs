867e4c1abb0b8805bbc1ef6584f15b7e4082ecdd
へーさん <hira9603859504@gmail.com>
2016-09-19 13:23:25 +0900

    GameManager追加、ドーナッツカウンター

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public int donuts = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
