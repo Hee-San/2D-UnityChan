ea869a9d2c1390e8c8a8224cea6eb5ecee2246c5
へーさん <hira9603859504@gmail.com>
2016-09-21 22:57:47 +0900

    UI　ドーナッツ・カウント

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour {
 
 	public int donuts = 0;
-
+	public GameObject canvas;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +13,6 @@ public class GameManagerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		canvas.GetComponent<Text> ().text = donuts.ToString ();
 	}
 }
