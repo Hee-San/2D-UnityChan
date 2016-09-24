0f0fd35318eb9334bddd670d53d690e71255bf1c
imanow <ima10324-127@outlook.jp>
2016-09-24 10:05:29 +0900

    UIHeartBox追加

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 public class GameManagerScript : MonoBehaviour {
 
 	public int donuts = 0;
-	public int candy=0;
-	public int pudding=0;
+	public int candy = 0;
+	public int pudding = 0;
+	public bool heart_1, heart_2, heart_3 ;
 	public GameObject donutstext;
 	public GameObject candytext;
 	public GameObject puddingtext;
+
+	void Awake(){
+		heart_1= false;
+		heart_2= false;
+		heart_3= false;
+	}
 
 	// Use this for initialization
 	void Start () {
